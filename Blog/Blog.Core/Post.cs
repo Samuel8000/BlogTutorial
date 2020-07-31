4b52348b8f72baa9c97ec68ccee9740e07bd496c
Samuel Ketels <ketels.samuel@gmail.com>
2020-07-31 17:30:43 +0200

    Post model

---
+++ Blog/Blog.Core/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Core
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime DatePosted { get; set; } = DateTime.Now;
+    }
+}
---
+++ Blog/Blog/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+            return RedirectToAction("Home", "Index");
+        }
+    }
+}
+
